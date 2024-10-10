64ea59910d5e823999cb1792083b4a73a7ebd07f
Babkina-Ekaterina <bkatya5577@yandex.ru>
2024-10-10 10:03:46 +0300

    Add client, contact info, contact type entities

---
+++ src/main/java/backend/test/client_storage/data/entity/Client.java
@@ -0,0 +1,28 @@
+package backend.test.client_storage.data.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "client")
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToMany(mappedBy = "client", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<ContactInfo> contactInfos = new HashSet<>();
+}
---
+++ src/main/java/backend/test/client_storage/data/entity/ContactInfo.java
@@ -0,0 +1,30 @@
+package backend.test.client_storage.data.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "contact_info")
+public class ContactInfo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id", nullable = false)
+    private Client client;
+
+    @ManyToOne
+    @JoinColumn(name = "contact_type_id", nullable = false)
+    private ContactType contactType;
+
+    @Column(name = "value")
+    private String value;
+}
