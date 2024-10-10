48f9083876d60ee9f035d0e4dd44a0cd31f13c75
Babkina-Ekaterina <bkatya5577@yandex.ru>
2024-10-10 10:04:25 +0300

    Add dto for client and contact type

---
+++ src/main/java/backend/test/client_storage/data/dto/ContactInfoAddDto.java
@@ -0,0 +1,43 @@
+package backend.test.client_storage.data.dto;
+
+import backend.test.client_storage.enumeration.ContactTypeEnum;
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class ContactInfoAddDto {
+    @NotNull(message = "Client id cannot be null")
+    @Min(value = 1, message = "Client id must be at least 1")
+    private Long clientId;
+    @NotNull(message = "Contact type cannot be null")
+    private String contactTypeName;
+    @NotNull(message = "Value cannot be null")
+    @NotBlank(message = "Value cannot be blank")
+    private String value;
+
+    @AssertTrue(message = "Contact type must be 'email' or 'phoneNumber'")
+    public boolean isValidContactTypeName() {
+        try {
+            ContactTypeEnum.fromValue(contactTypeName);
+            return true;
+        } catch (IllegalArgumentException ex) {
+            return false;
+        }
+    }
+
+    @AssertTrue(message = "Contact value must be a valid string depending on contact type")
+    public boolean isValidValue() {
+        try {
+            ContactTypeEnum type = ContactTypeEnum.fromValue(contactTypeName);
+            return type.isValid(value);
+        } catch (IllegalArgumentException ex) {
+            return false;
+        }
+    }
+}
