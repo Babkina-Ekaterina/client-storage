a1ef36f226e262143d23e99ba486343e90b1690b
Babkina-Ekaterina <bkatya5577@yandex.ru>
2024-10-10 13:23:07 +0300

    Add validation for contact type

--- src/main/java/backend/test/client_storage/data/dto/ContactTypeAddDto.java
+++ src/main/java/backend/test/client_storage/data/dto/ContactTypeAddDto.java
@@ -1,5 +1,8 @@
 package backend.test.client_storage.data.dto;
 
+import backend.test.client_storage.enumeration.ContactTypeEnum;
+import jakarta.validation.constraints.AssertTrue;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,5 +13,16 @@ import lombok.Setter;
 @Getter
 @Setter
 public class ContactTypeAddDto {
+    @NotNull(message = "Contact type cannot be null")
     private String name;
+
+    @AssertTrue(message = "Contact type must be 'email' or 'phoneNumber'")
+    public boolean isValidContactTypeName() {
+        try {
+            ContactTypeEnum.fromValue(name);
+            return true;
+        } catch (IllegalArgumentException ex) {
+            return false;
+        }
+    }
 }
